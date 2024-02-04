131a2df023ee6969603b07a81574f0742b731e6a
shaileshdevs <shaileshdevs@gmail.com>
2024-02-04 21:50:44 +0530

    Include screenshots

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'MAIN_FILE_PATH', __FILE__ );
-define( 'DOMAIN_SUB_PART', 'nec' ); // If your website's home url is https://yourdomain.com/en, then you must provide the 'en' in this constant value.
+define( 'DOMAIN_SUB_PART', 'nec' ); // If your website's home url is https://yourdomain.com/en, then you must provide the 'en' in this constant value. If your website’s home url is https://yourdomain.com/, then you must enter empty string like this ‘’.
 define( 'DB_HOST', 'localhost' );
 define( 'DB_USERNAME', 'admin' );
 define( 'DB_PASSWORD', 'admin' );
